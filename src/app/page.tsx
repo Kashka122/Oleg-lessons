350a2e4d72ba246b8d622741ddd7c322fda3480f
Oleg Kashentsev <okashentsev@gmail.com>
2025-01-02 02:34:14 +0500

    made modal window, added styles to project.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 'use client'
-
+import { Modal } from '../components/Modal/Modal';
+import styles from './page.module.css';
 import { useEffect, useState } from "react";
+import Image from 'next/image';
 
 type List = Array<{
   id: number
@@ -9,9 +11,11 @@ type List = Array<{
 }>
 
 export default function Home() {
-  const [posts, setPosts] = useState<List>([])
-  const [error, setError] = useState(null)
-  const [loading, setLoading] = useState(true)
+  const [posts, setPosts] = useState<List>([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [modalActive, setModalActive] = useState(false);
+  const [idForModal, setIdForModal] = useState(0);
 
   useEffect(() => {
     setLoading(true)
@@ -23,22 +27,41 @@ export default function Home() {
       .finally(() => setLoading(false))
   }, [])
 
-  if (error) {
-    return <div>{String(error)}</div>
-  }
+  const renderContent = () =>{
+    if (error) {
+      return (
+      <>
+          <Image src="/cross.svg" alt="Error sign" width={128} height={128} />
+          <div className={styles.error}>{String(error)}</div>
+      </>
+      )
+    }
+  
+    if (loading) {
+        return (
+         <div className={styles.spinner}></div>
+            )
+    }
 
-  if (loading) {
-    return <div>Loading...</div>
+    return(
+      <>{posts.map(item => (
+        <div key={item.id} className={styles.post}>
+          <p className={styles.title}>{item.title}</p>
+          <p className={styles.text}>{item.body}</p>
+          <button className={styles.btn} onClick={()=> {
+            setModalActive(true);
+            setIdForModal(item.id)}} >Подробнее</button>
+        </div>
+      ))}</>
+      
+    )
   }
+  
 
   return (
-    <div>
-      {posts.map(item => (
-        <div key={item.id}>
-          <p>{item.title}</p>
-          <p>{item.body}</p>
-        </div>
-      ))}
+    <div className={styles.container}>
+      {renderContent()}
+      <Modal active={modalActive} setActive={setModalActive} id={idForModal} />
     </div>
   );
 }
--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,3 +1,58 @@
-export function Modal() {
-    return <div>test</div>
+import Image from 'next/image';
+import styles from './Modal.module.css'
+import { useState, useEffect } from 'react'
+
+interface ModalProps { // made interface for props, to avoid warning of linter about props we've received
+  active: boolean;
+  setActive: (active: boolean) => void;
+  id?: number;
+}
+
+export function Modal({active, setActive, id= 0}: ModalProps) {
+      const [post, setPost] = useState<{ title: string; body: string } | null>(null);// made types for post element, to avoid warning of linter about types on 44-45 lines
+      const [error, setError] = useState(null)
+      const [loading, setLoading] = useState(true)
+
+      useEffect(() => {
+          setLoading(true)
+      
+          fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+            .then(response => response.json())
+            .then(value=> setPost(value))
+            .catch((error) => setError(error))
+            .finally(() => setLoading(false))
+        }, [id])
+        
+        const renderContent = () =>{
+            if (error) {
+                return (
+                <>
+                    <Image src="/cross.svg" alt="Error sign" width={128} height={128} />
+                    <div className={styles.error}>{String(error)}</div>
+                </>
+                )
+              }
+            
+              if (loading) {
+                  return (
+                   <div className={styles.spinner}></div>
+                      )
+              }
+
+              return (
+                <>
+                    <p className={styles.title}>{post ? post.title : ''}</p>
+                    <p className={styles.body}>{post ? post.body : ''}</p>
+                </>
+                    )
+        }
+        
+
+    return (
+        <div className={active ? `${styles.modal} ${styles.active}` : styles.modal} onClick={()=> setActive(false)} >   
+                <div className={styles.modal__content} onClick={e=> e.stopPropagation()}>
+                    {renderContent()}
+                </div>
+        </div>
+    )
 }
