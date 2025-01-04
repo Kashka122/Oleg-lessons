a462ab2a8b3066e317434cbbd169b2141495fcf6
Oleg Kashentsev <okashentsev@gmail.com>
2025-01-04 23:28:00 +0500

    implemented prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
-'use client'
+'use client';
 import { Modal } from '../components/Modal/Modal';
 import styles from './page.module.css';
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 import Image from 'next/image';
 
 type List = Array<{
-  id: number
-  title: string
-  body: string
-}>
+  id: number;
+  title: string;
+  body: string;
+}>;
 
 export default function Home() {
   const [posts, setPosts] = useState<List>([]);
@@ -18,45 +18,49 @@ export default function Home() {
   const [idForModal, setIdForModal] = useState(0);
 
   useEffect(() => {
-    setLoading(true)
+    setLoading(true);
 
     fetch('https://jsonplaceholder.typicode.com/posts')
-      .then(response => response.json())
-      .then(value => setPosts(value))
+      .then((response) => response.json())
+      .then((value) => setPosts(value))
       .catch((error) => setError(error))
-      .finally(() => setLoading(false))
-  }, [])
+      .finally(() => setLoading(false));
+  }, []);
 
-  const renderContent = () =>{
+  const renderContent = () => {
     if (error) {
       return (
-      <>
+        <>
           <Image src="/cross.svg" alt="Error sign" width={128} height={128} />
           <div className={styles.error}>{String(error)}</div>
-      </>
-      )
-    }
-  
-    if (loading) {
-        return (
-         <div className={styles.spinner}></div>
-            )
+        </>
+      );
     }
 
-    return(
-      <>{posts.map(item => (
-        <div key={item.id} className={styles.post}>
-          <p className={styles.title}>{item.title}</p>
-          <p className={styles.text}>{item.body}</p>
-          <button className={styles.btn} onClick={()=> {
-            setModalActive(true);
-            setIdForModal(item.id)}} >Подробнее</button>
-        </div>
-      ))}</>
-      
-    )
-  }
-  
+    if (loading) {
+      return <div className={styles.spinner}></div>;
+    }
+
+    return (
+      <>
+        {posts.map((item) => (
+          <div key={item.id} className={styles.post}>
+            <p className={styles.title}>{item.title}</p>
+            <p className={styles.text}>{item.body}</p>
+            <button
+              className={styles.btn}
+              onClick={() => {
+                setModalActive(true);
+                setIdForModal(item.id);
+              }}
+            >
+              Подробнее
+            </button>
+          </div>
+        ))}
+      </>
+    );
+  };
 
   return (
     <div className={styles.container}>
